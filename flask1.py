e86e413627245ffd281d2477300017f99766c2ae
zengdewei1 <2313970511@qq.com>
2018-11-22 19:01:29 +0800

    11.22 json well

--- flask1.py
+++ flask1.py
@@ -2,6 +2,7 @@
 
 from flask import Flask,render_template,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 app=Flask(__name__)
 
@@ -9,6 +10,12 @@ app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:zdw20001209@localhos
 app.config.from_pyfile("./settings.py")
 
 db = SQLAlchemy(app)
+
+def to_dict(self):
+        return{c.name:getattr(self,c.name,None) for c in self.__table__.columns}
+
+# def to_json():
+
 
 class Admin(db.Model):
         __tablename__='admins'
@@ -27,13 +34,33 @@ class Door(db.Model):
         address=db.Column(db.String(32),unique=True)
         # def __repr__(self):
         #         return'<Role:%s %s %s %s>' % (self.name ,self.id,self.email,self.password)
-class Histoty(db.Model):
+
+class History(db.Model):
         __tablename__='historys'
         id=db.Column(db.Integer,primary_key=True)
         doorName=db.Column(db.String(32),db.ForeignKey('doors.doorName'))
         adminName=db.Column(db.String(32),db.ForeignKey('admins.adminName'))#外键
         right = db.Column(db.String(32))
         charge = db.Column(db.String(32))
+        def to_json(self):
+                json_history={
+                        'doorName':self.doorName,
+                        'adminName':self.adminName,
+                        'right':self.right,
+                        'charge':self.charge,
+                }
+                return json_history
+# door1=Door(doorName="10",address="127.0.0.1")
+# db.session.add(door1)
+# db.session.commit()
+# admin1=Admin(adminName="zdw",password="123",email="121")
+# db.session.add(admin1)
+# db.session.commit()
+# his1=Histoty(doorName="10",adminName="zdw",right="user")
+# his2=Histoty(doorName="10",adminName="zdw",right="user")
+# db.session.add_all([his1,his2])
+# db.session.add(his1)
+# db.session.commit()
 
 @app.route('/')
 def index():
@@ -150,6 +177,27 @@ def addPdoor():
         # db.session.add(door)
         # db.session.commit()
         return 'success'
+
+@app.route('/admindoor')
+def searchAdminDoor():
+        # admin=request.form.get('name')
+        historys=History.query.filter_by(adminName="zdw").all()
+        # history=history.to_dict()
+        # history=history.__dict__
+        # history=jsonify(history)
+        list=[]
+        for history in historys:
+                history=history.to_json()
+                print(history)
+                history=jsonify(history)
+                list.append(history)
+                print(history.data)
+        # list=jsonify(list)
+        print(list[0].data)
+        # print('yes')
+        # return history
+        return "admindoor"
+
 
 if __name__== '__main__':
         # db.drop_all()
